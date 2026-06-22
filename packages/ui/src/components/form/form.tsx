86f3da8682b571de8ad388b585aa73bbbfc17947
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-06-22 20:43:07 +0200

    remove unused prop

--- packages/ui/src/components/form/form.tsx
+++ packages/ui/src/components/form/form.tsx
@@ -2,10 +2,6 @@ import * as Aria from "react-aria-components/Form";
 
 interface FormProps extends Aria.FormProps {}
 
-export function Form({ children, onSubmit, ...rest }: FormProps) {
-  return (
-    <Aria.Form onSubmit={onSubmit} {...rest}>
-      {children}
-    </Aria.Form>
-  );
+export function Form({ children, ...rest }: FormProps) {
+  return <Aria.Form {...rest}>{children}</Aria.Form>;
 }
