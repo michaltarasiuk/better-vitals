30bc7a70fcd608a81ac4847b06b53c95e4edfc8f
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-06-24 13:30:18 +0200

    add organization invitation accept route

---
+++ apps/dashboard/app/routes/organization.$slug_.invite.accept/route.ts
@@ -0,0 +1,38 @@
+import { isDefined } from "@lite-app/shared/is-defined";
+import { href, redirect } from "react-router";
+
+import { auth } from "~/lib/auth/index.server";
+import { isLoggedIn } from "~/lib/auth/session.server";
+
+import type { Route } from "./+types/route";
+
+export async function loader({ params, request, url }: Route.LoaderArgs) {
+  const id = url.searchParams.get("id");
+  if (!isDefined(id)) {
+    throw new Response("Bad Request", { status: 400 });
+  }
+  const loggedIn = await isLoggedIn(request);
+  if (!loggedIn) {
+    throw redirect(
+      `${href("/signup")}?${new URLSearchParams({
+        invitationId: id,
+        organizationSlug: params.slug,
+      })}`
+    );
+  }
+  try {
+    await auth.api.acceptInvitation({
+      body: {
+        invitationId: id,
+      },
+      headers: request.headers,
+    });
+  } catch {
+    throw new Response("Bad Request", { status: 400 });
+  }
+  throw redirect(
+    href("/organization/:slug", {
+      slug: params.slug,
+    })
+  );
+}
